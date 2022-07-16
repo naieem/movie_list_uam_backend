d7fd3437140a22fa7ec08e603e26024a83aec292
Naieem Mahmud Supto <naieemsupto@gmail.com>
2022-07-17 02:26:54 +0700

    config module added to auth module

--- src/uam/auth/auth.module.ts
+++ src/uam/auth/auth.module.ts
@@ -5,9 +5,11 @@ import { AuthController } from './auth.controller';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { UsersModule } from '../users/users.module';
 import { PassportModule } from '@nestjs/passport';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule,
     PassportModule.register({ defaultStrategy: 'jwt', session: false }),
     JwtModule.register({
       secretOrPrivateKey: 'givethisanewsecret',
